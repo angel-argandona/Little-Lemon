b462fd7a9fb796d9e227719800460a3aec961c7b
Angel Argandona <angel.argandonavite@gmail.com>
2025-02-27 16:02:03 -0800

    Added JS components

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			<img href=""></img>
+			<Nav/>
+			<div>
+				<h2>Contact</h2>
+				<p>Adress</p>
+				<p>Phone number</p>
+				<p>Email</p>
+			</div>
+			<div>
+				<h2>Social Media Links</h2>
+				<p>Links here</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer;
