a290ca58898063cb389461262e1f57823cdcad90
Angel Argandona <angel.argandonavite@gmail.com>
2025-03-03 01:13:04 -0800

    Finalized transition to appropriate units of measurement and completed Specials section

--- src/components/DishCard.js
+++ src/components/DishCard.js
@@ -3,13 +3,17 @@ import React from "react";
 const DishCard = ({name, price, desc, relPath}) => {
 	
 	return (
-		<div>
+		<div className="card">
 			<img className="dish-picture" src={require(`../images/${relPath}`)}></img>
-			<h5>{name}</h5>
-			<p className="highlight-text">{price}</p>
+			<div className="card-title">
+				<h5>{name}</h5>
+				<p className="highlight-text price">{price}</p>
+			</div>
 			<p>{desc}</p>
-			<p className="highlight-text">Order a delivery</p>
-			<img className="delivery-logo" src={require("../images/delivery-icon.jpg")}></img>
+			<div className="delivery-call">
+				<p className="highlight-text">Order a delivery</p>
+				<img className="delivery-icon" src={require("../images/delivery-icon.jpg")}></img>
+			</div>
 		</div>
 	)
 }
